b9d3bd96fe69ce0e14858d55795d676eb541feff
Iskhakov <tim.iskchackov@gmail.com>
2018-10-19 19:41:57 +0700

    Sozdali ssilku

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__) # __main__
 
 
 @app.route('/')
 def index() :
-    return("Hello world!")
+    with app.test_request_context():
+        link = url_for('hello_user',username='Danil is host')
+
+    return render_template('index.html',link=link)
 
 
-@app.route('/user/<path:username>')
-def hello_user(username):
-    return "Hello %s" % username
-
-
-with app.test_request_context():
-    print("http://localhost" + url_for('hello_user', username='Danil host'))
+@app.route('/user/')
+@app.route('/user/<username>')
+def hello_user(username=None):
+    numbers = [1,2,3,4,5,6,7,8,9,0]
+    return render_template(
+            'user.html',
+            username=username,
+            numbers=numbers
+    )
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
